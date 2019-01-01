506ba09101aa86a457d2410c12dc48d2a96e634f
John Plaschke <plaschkej@gmail.com>
2019-01-01 15:16:59 -0500

    Add order table to admin

--- diocese/admin.py
+++ diocese/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Diocese, Archdiocese, Statistics, Priest
+from .models import Diocese, Archdiocese, Statistics, Priest, Order
 
 admin.site.register(Diocese)
 admin.site.register(Archdiocese)
 admin.site.register(Statistics)
 admin.site.register(Priest)
+admin.site.register(Order)
 
